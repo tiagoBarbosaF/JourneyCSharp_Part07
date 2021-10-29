04bc16ce8dc5c64b4e956bcf4b3160cfc7a74058
Tiago Barbosa <tiago@farias.cc>
2021-10-29 18:29:33 -0300

    Journey CSharp Part07 - Array and Generics types

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays de inteiro
+
+            int[] idades = new int[] {20,25,30,35,40};
+
+            //idades[0] = 20;
+            //idades[1] = 25;
+            //idades[2] = 30;
+            //idades[3] = 35;
+            //idades[4] = 40;
+
+            Console.WriteLine(idades[1]);
+            Console.WriteLine(idades[2]);
+
+            //int indice = 2;
+            //int valorNoIndice = idades[indice];
+            int outroValor = idades[2+2];
+            //int maisUmValor = idades[GetNumeroInteiro()];
+
+            int acumulador = 0;
+
+            for( int indice = 0; indice < idades.Length; indice++ )
+            {
+                int idade = idades[indice];
+                acumulador += idades[indice];
+                Console.WriteLine($"Valor da idade no índice {indice} é {idade}");
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades: {media}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Array e tipos genéricos
+
+## Objetivos ao completar os estudos
+
+- Conheça os arrays
+- Crie uma lista dinâmica
+- Use tipos genéricos
+- Aprenda como usar o argumentos params
+- Use Argumentos opcionais e nomeados
+- Crie indexadores em classes
+
+## Ementas:
+
+### Conhecendo Arrays
+
+
+### Criando nossa Lista
+
+
+### Criando o método Remover
+
+
+### Indexadores e argumentos params
+
+
+### Começando com genéricos
